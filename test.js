fda6c153d9a8e7abeb0e01c617d2d7a18ba78d66
SOMNATH SAHOO <78023748+SOMANATH-SAHOO@users.noreply.github.com>
2024-10-13 18:01:48 +0530

    Update test.js

--- test.js
+++ test.js
@@ -313,3 +313,20 @@ for (let i = 0; i < arr.length; i++) {
         console.log("LEADER IS", arr[i])
     }
 }
+
+------------------------XXXXX----------------------
+
+
+function findMissingNumber(arr) {
+  const n = arr.length + 1;
+  const totalSum = (n * (n + 1)) / 2;
+  const actualSum = arr.reduce((acc, num) => acc + num, 0);
+  return totalSum - actualSum;
+}
+
+
+const arr = [1, 2, 4, 5, 6];
+console.log(findMissingNumber(arr));  
+
+
+-------------------------XXXX-----------------------
